93baebac61e3e1463707d0227b86774b4bfb250c
William Ngiam <wngi5916@uni.sydney.edu.au>
2016-06-01 15:45:40 +1000

    Updated data analysis code
    Data analysis code has been updated to compile to calculate VWM
    capacity (K) in each block, as well as produce figures of the number of
    correct trials and K plotted over blocks. The code also counts the
    number of low probability pairs included in each pattern condition
    block, and counts how many trials contained only high probability pairs.

--- VWMStatisticalLearning_Analysis.m
+++ VWMStatisticalLearning_Analysis.m
@@ -1,4 +1,17 @@
 % VWM Statistical Learning Analysis
+%
+% WXQN started writing this on 13th June 2016
+% This code is for data analysis of VWMStatisticalLearning.m
+%
+% Currently this code:
+%
+% * Compiles the number of correct trials in each block for each participant
+% * Calculates the percentage correct and VWM capacity (K) in each block
+% * Plots percentage correct and K over blocks for each participant
+% * Counts the number of trials including low probability pairs in each
+%   pattern condition block for each participant
+
+% ----------------------------------------------------------------------- %
 
 cd('/Users/alexh/Documents/MATLAB/William/VWMStatisticalLearning/Data/');
 
@@ -6,40 +19,55 @@ theseFiles = what;
 theseFiles = theseFiles.mat;
 nFiles = numel(theseFiles);
 load(theseFiles{1});
+participantID = [];
 
-participantID = {'02'};
+for thisFile = 1:nFiles
+
+load(theseFiles{thisFile});
+thisID = participant.ID;
+
+participantID = [participantID; thisID];
+
+end
+
+participantID = unique(cellstr(participantID))';
 numParticipants = numel(participantID);
+    
+% Compile responses
+nCorr = NaN(numParticipants,20);
+pCorr = NaN(numParticipants,20);   
+K = NaN(numParticipants,20);
+
+nCorr_unif = NaN(numParticipants,10);
+pCorr_unif = NaN(numParticipants,10);
+K_unif = NaN(numParticipants,10);
+
+nCorr_patt = NaN(numParticipants,10);
+pCorr_patt = NaN(numParticipants,10);
+K_patt = NaN(numParticipants,10);
+
 
 for thisNum = 1:numParticipants
-    
-     % Compile responses
-    nCorr = NaN(1,20);
-    pCorr = NaN(1,20);   
-    
+
     for thisFile = 1:nFiles
 
         % Load the file
         load(theseFiles{thisFile});
-        participantID = {'02'};
         thisParticipantID = participantID{thisNum};
         thisID = participant.ID;
         thisBlock = block.thisBlock; 
         
         if strcmp(thisID,thisParticipantID)
         
-            nCorr(thisBlock) = sum(block.allCorrect);
-            pCorr(thisBlock) = sum(block.allCorrect)/experiment.nTrialsPerBlock;
-
+            nCorr(thisNum,thisBlock) = sum(block.allCorrect);
+            pCorr(thisNum,thisBlock) = sum(block.allCorrect)/experiment.nTrialsPerBlock;
+            K(thisNum,thisBlock) = (64*pCorr(thisNum,thisBlock)-8)/7;
         end
         
     end
      
     disp(nCorr);
-    nCorr_unif = NaN(1,10);
-    pCorr_unif = NaN(1,10);
-    nCorr_patt = NaN(1,10);
-    pCorr_patt = NaN(1,10);
-
+   
     thisParticipant = participantID{thisNum};
     firstFileName = [thisParticipant '_VWMStatLearning_1.mat'];
     load(firstFileName);
@@ -51,14 +79,16 @@ for thisNum = 1:numParticipants
             if mod(thisBlock,2) == 1
                 
                 thisConditionBlock = (thisBlock+1)/2;
-                nCorr_unif(thisConditionBlock) = nCorr(thisBlock);
-                pCorr_unif(thisConditionBlock) = pCorr(thisBlock);
+                nCorr_unif(thisNum,thisConditionBlock) = nCorr(thisNum,thisBlock);
+                pCorr_unif(thisNum,thisConditionBlock) = pCorr(thisNum,thisBlock);
+                K_unif(thisNum,thisConditionBlock) = K(thisNum,thisBlock);
                 
             elseif mod(thisBlock,2) == 0
                 
                 thisConditionBock = thisBlock/2;
-                nCorr_patt(thisConditionBlock) = nCorr(thisBlock);
-                pCorr_patt(thisConditionBlock) = pCorr(thisBlock);
+                nCorr_patt(thisNum,thisConditionBlock) = nCorr(thisNum,thisBlock);
+                pCorr_patt(thisNum,thisConditionBlock) = pCorr(thisNum,thisBlock);
+                K_patt(thisNum,thisConditionBlock) = K(thisNum,thisBlock);
                 
             end
             
@@ -71,14 +101,16 @@ for thisNum = 1:numParticipants
             if mod(thisBlock,2) == 0
                 
                 thisConditionBlock = (thisBlock)/2;
-                nCorr_unif(thisConditionBlock) = nCorr(thisBlock);
-                pCorr_unif(thisConditionBlock) = pCorr(thisBlock);
+                nCorr_unif(thisNum,thisConditionBlock) = nCorr(thisNum,thisBlock);
+                pCorr_unif(thisNum,thisConditionBlock) = pCorr(thisNum,thisBlock);
+                K_unif(thisNum,thisConditionBlock) = K(thisNum,thisBlock);
                 
             elseif mod(thisBlock,2) == 1
                 
                 thisConditionBlock = (thisBlock+1)/2;
-                nCorr_patt(thisConditionBlock) = nCorr(thisBlock);
-                pCorr_patt(thisConditionBlock) = pCorr(thisBlock);
+                nCorr_patt(thisNum,thisConditionBlock) = nCorr(thisNum,thisBlock);
+                pCorr_patt(thisNum,thisConditionBlock) = pCorr(thisNum,thisBlock);
+                K_patt(thisNum,thisConditionBlock) = K(thisNum,thisBlock);
                 
             end
             
@@ -86,34 +118,88 @@ for thisNum = 1:numParticipants
 
     end
     
+    Conditions = {'Uniform', 'Pattern'};
     figure('Color','white','Name',thisParticipantID);
     subplot(2,1,1);
-    plot(nCorr_unif,'bx-');
+    plot(nCorr_unif(thisNum,:),'bx-');
+    xlabel('Block Number');
+    ylabel('Number of Correct Trials');
+    set(gca,'TickDir', 'out')
+    set(gca,'YMinorTick', 'on')
     hold on;
-    plot(nCorr_patt,'rx-');
-
+    plot(nCorr_patt(thisNum,:),'rx-');
+    legend(Conditions,'Location','NorthOutside');
+    
     subplot(2,1,2);
-    plot(pCorr_unif,'bx-');
+    plot(K_unif(thisNum,:),'bx-');
+    xlabel('Block Number');
+    ylabel('Capacity (K)');
+    set(gca,'TickDir', 'out')
+    set(gca,'YMinorTick', 'on')
     hold on;
-    plot(pCorr_patt,'rx-');
+    plot(K_patt(thisNum,:),'rx-');
     hold off;
+    legend(Conditions,'Location','NorthOutside');
+       
+end
+
+% Combined figure of all participants (better for low n)
+
+for thisParticipant = 1:numParticipants
+    
+    subplot(numParticipants,2,thisParticipant*2-1);
+    plot(nCorr_unif(thisParticipant,:),'bx-');
+    xlabel('Block Number');
+    ylabel('Number of Correct Trials');
+    set(gca,'TickDir', 'out')
+    set(gca,'YMinorTick', 'on')
+    hold on;
+    plot(nCorr_patt(thisParticipant,:),'rx-');
+    legend(Conditions,'Location','NorthOutside');
+    
+    subplot(numParticipants,2,thisParticipant*2);
+    plot(K_unif(thisParticipant,:),'bx-');
+    xlabel('Block Number');
+    ylabel('Capacity (K)');
+    set(gca,'TickDir', 'out')
+    set(gca,'YMinorTick', 'on')
+    hold on;
+    plot(K_patt(thisParticipant,:),'rx-');
+    hold off;
+    legend(Conditions,'Location','NorthOutside');
     
 end
 
-cd('/Users/alexh/Documents/MATLAB/William/VWMStatisticalLearning/PilotData/');
+% Figure of mean data
 
-theseFiles = what;
-theseFiles = theseFiles.mat;
-nFiles = numel(theseFiles);
-load(theseFiles{1});
+figure;
+subplot(2,1,1);
+plot(mean(nCorr_unif),'bx-');
+xlabel('Block Number');
+ylabel('Mean Number of Correct Trials');
+set(gca,'TickDir','out');
+set(gca,'YMinorTick','on');
+hold on;
+plot(mean(nCorr_patt),'rx-');
+legend(Conditions,'Location','NorthOutside');
 
-participantID = {'02'};
-numParticipants = numel(participantID);
+subplot(2,1,2);
+plot(mean(K_unif),'bx-');
+xlabel('Block Number');
+ylabel('Mean K');
+set(gca,'TickDir','out');
+set(gca,'YMinorTick','on');
+hold on;
+plot(mean(K_patt),'rx-');
+legend(Conditions,'Location','NorthOutside');
+
+data.allLowProbPairsNum = NaN(numParticipants,9);
+data.allHighProbPairTrialsNum = NaN(numParticipants,9);
 
 for thisParticipantNum = 1:numParticipants
         
-    data.allLowProbPairsNum = [];
-    data.allHighProbPairTrialsNum = [];
+    allLowProbPairsNum = [];
+    allHighProbPairTrialsNum = [];
     
     for thisFile = 1:nFiles
 
@@ -132,30 +218,37 @@ for thisParticipantNum = 1:numParticipants
 
         end
         
-        if thisCondition == 2
+        if strcmp(thisID,thisParticipantID)
         
-            for thisPair = 1:stimulus.nPairs
+            if thisCondition == 2
 
-                thisOne = stimulus.highProbPairs(thisPair);
+                for thisPair = 1:stimulus.nPairs
 
-                a = a + (block.allPairs == thisOne);
+                    thisOne = stimulus.highProbPairs(thisPair);
+
+                    a = a + (block.allPairs == thisOne);
+
+                end
+    %           disp('a');
+    %           disp(a);
+
+                disp('nLowProbPairs');
+                nLowProbPairs = numel(find(a == 0));
+                disp(nLowProbPairs);
+                allLowProbPairsNum = [allLowProbPairsNum nLowProbPairs];
+
+                disp('nHighProbPairTrials');
+                nHighProbPairTrials = sum(sum(a')==4);
+                disp(nHighProbPairTrials);
+                allHighProbPairTrialsNum = [allHighProbPairTrialsNum nHighProbPairTrials];
 
             end
-            disp('a');
-            disp(a)
-
-            disp('nLowProbPairs');
-            nLowProbPairs = numel(find(a == 0));
-            disp(nLowProbPairs);
-            data.allLowProbPairsNum = [data.allLowProbPairsNum nLowProbPairs];
-
-            disp('nHighProbPairTrials');
-            nHighProbPairTrials = sum(sum(a')==4);
-            disp(nHighProbPairTrials);
-            data.allHighProbPairTrialsNum = [data.allHighProbPairTrialsNum nHighProbPairTrials];
 
         end
-
+        
     end
+    
+    data.allLowProbPairsNum(thisParticipantNum,:) = allLowProbPairsNum;
+    data.allHighProbPairTrialsNum(thisParticipantNum,:) = allHighProbPairTrialsNum;
     
 end
